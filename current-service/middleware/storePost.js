a03adcd021b53fbb97527d2209e518bca56372b7
Zachary Perales <aardzarks@gmail.com>
2023-03-13 23:33:23 -0400

    Remove obsolete middleware files

--- current-service/middleware/storePost.js
+++
@@ -1,9 +0,0 @@
-// OBSOLETE. KEPT FOR DEMONSTRATION PURPOSES.
-
-module.exports = (req, res, next) => {
-    if ( !req.body.username || !req.body.title || !req.body.description || !req.body.content) {
-        return res.redirect('/posts/new');
-    }
-
-    next();
-}
