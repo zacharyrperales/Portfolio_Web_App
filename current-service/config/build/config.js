5efe046aec772b911079e6034c1bde5e457e7c54
Zachary Perales <aardzarks@gmail.com>
2023-03-28 16:49:54 -0400

    Configure environment variables for GitHub API

--- current-service/config/build/config.js
+++ current-service/config/build/config.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 
 // ENVIRONMENT VARIABLES
 const PORT = parseInt(process.env.PORT) || 8080;
+const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
 module.exports = {
-    PORT
+    PORT,
+    GITHUB_USERNAME,
+    GITHUB_TOKEN
 }
