2e9a175144c6c27321e8f65e4e12090e23f404a5
Zachary Perales <aardzarks@gmail.com>
2023-03-28 16:49:34 -0400

    Import and test GitHub API controller

--- current-service/server.js
+++ current-service/server.js
@@ -1,13 +1,18 @@
-const favicon =         require('serve-favicon');
-const path =            require('path');
-const express =         require('express');
-const indexController = require('./controllers/indexController');
-const { PORT } =        require('./config/build/config.js');
-
+const favicon           = require('serve-favicon');
+const path              = require('path');
+const express           = require('express');
+const indexController   = require('./controllers/indexController');
+const { PORT }          = require('./config/build/config.js');
+const githubController  = require('./controllers/githubController.js');
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/views/resources/favicon")));
+
+// Path for potential future local stylesheet to replace css inside view
 // app.use(express.static(path.join(__dirname, "/views/resources/css")));
+
+// Test GitHub API connection
+// console.log(githubController.github.getUser());
 
 app.set('view engine', 'ejs');
 
