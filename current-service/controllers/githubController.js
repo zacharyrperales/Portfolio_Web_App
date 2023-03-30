f1505ecf944531236810612f9da72bea622fd38c
Zachary Perales <aardzarks@gmail.com>
2023-03-29 23:55:19 -0400

    Add retrieval logic for testing page

--- current-service/controllers/githubController.js
+++ current-service/controllers/githubController.js
@@ -1,11 +1,16 @@
 const GitHub                            = require('github-api');
 const { GITHUB_USERNAME, GITHUB_TOKEN } = require('../config/build/config.js');
 
-const github = new GitHub({
+const gitHub = new GitHub({
     username: GITHUB_USERNAME,
     token: GITHUB_TOKEN
 });
 
+const getGitHubPage = async (req, res) => {
+    res.render("pages/testing.ejs");
+}
+
 module.exports = {
-    github
+    gitHub,
+    getGitHubPage
 }
